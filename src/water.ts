21297f392ebc6485c97638e2454a2f2f62252770
nevermakeport <nevermakeport@users.noreply.github.com>
2017-07-22 15:21:20 +0300

    Sum wavefronts by frequency groups

--- src/water.ts
+++ src/water.ts
@@ -22,21 +22,22 @@ const clock = new THREE.Clock()
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
 
 class Wavefront {
-  constructor(public amplitude: number,
+  constructor(public amplitudeOverLength: number,
               public length: number,
               public steepness: number,
+              public speed: number,
               public direction: THREE.Vector2) {}
 }
 
 const wavefronts = [
-  new Wavefront(1, 10, 0.3, new THREE.Vector2(1, 1)),
-  new Wavefront(1, 10, 1, new THREE.Vector2(0.4, 0.8)),
-  new Wavefront(3, 30, 1, new THREE.Vector2(-0.6, 0.3)),
-  new Wavefront(3, 30, 1, new THREE.Vector2(-0.6, 0.3)),
-  new Wavefront(3, 30, 1, new THREE.Vector2(-0.6, 0.3)),
-  new Wavefront(1, 10, 0.3, new THREE.Vector2(1, 1)),
-  new Wavefront(1, 10, 1, new THREE.Vector2(0.4, 0.8)),
-  new Wavefront(3, 30, 1, new THREE.Vector2(-0.6, 0.3)),
+  new Wavefront(0.3, 15, 1, -1, degToVec2(0)),
+  new Wavefront(0.3, 25, 1, -1, degToVec2(120)),
+  new Wavefront(0.3, 45, 1, -1, degToVec2(240)),
+  new Wavefront(0.1, 5, 0.6, 1.7, degToVec2(0)),
+  new Wavefront(0.1, 7, 0.9, 1.5, degToVec2(72)),
+  new Wavefront(0.1, 5, 0.6, 1.3, degToVec2(144)),
+  new Wavefront(0.1, 7, 0.9, 1.1, degToVec2(216)),
+  new Wavefront(0.1, 5, 0.6, 1, degToVec2(288)),
 ]
 
 
@@ -51,14 +52,17 @@ wavefronts.forEach((wavefront, index) => {
 
   const folder = gui.addFolder(`Wave ${index + 1}`)
 
-  folder.add(GUIWavefront, 'amplitude')
-    .onChange((val: number) => { wavefront.amplitude = val })
+  folder.add(GUIWavefront, 'amplitudeOverLength').min(0.01).max(0.5).step(0.01)
+    .onChange((val: number) => { wavefront.amplitudeOverLength = val })
 
-  folder.add(GUIWavefront, 'length')
+  folder.add(GUIWavefront, 'length').min(1)
     .onChange((val: number) => { wavefront.length = val })
 
   folder.add(GUIWavefront, 'steepness').min(0).max(1).step(0.01)
     .onChange((val: number) => { wavefront.steepness = val })
+
+  folder.add(GUIWavefront, 'speed').step(0.1)
+    .onChange((val: number) => { wavefront.speed = val })
 
   folder.add(GUIWavefront, 'direction').min(0).max(359)
     .onChange((val: number) => { wavefront.direction = degToVec2(val) })
@@ -87,6 +91,8 @@ const surface = new THREE.Mesh(
     vertexShader: `
       #define M_PI 3.1415926535897932384626433832795
       #define NUM_WAVEFRONTS 8
+      #define NUM_HF_WAVEFRONTS 3
+      #define NUM_LF_WAVEFRONTS 5
 
       uniform float time;
 
@@ -99,28 +105,29 @@ const surface = new THREE.Mesh(
       };
 
       struct Wavefront {
-        float amplitude;
+        float amplitudeOverLength;
         float length;
         float steepness;
+        float speed;
         vec2 direction;
       };
       uniform Wavefront wavefronts[NUM_WAVEFRONTS];
 
-      Wave getWave(Wavefront wf) {
-        float A = wf.amplitude;
+      Wave getWave(Wavefront wf, vec3 basePosition) {
+        float A = wf.amplitudeOverLength * wf.length;
         float L = wf.length;
         float w = 2.0 * M_PI / L; // frequency
         float Q = wf.steepness / (w * A);
         vec2 D = wf.direction;
-        float speed = 1.0;        // phase
+        float speed = wf.speed;   // phase
 
-        float dotD = dot(position, vec3(D.x, 0.0, D.y));
+        float dotD = dot(basePosition, vec3(D.x, 0.0, D.y));
         float S = sin(w * dotD + time * speed);
         float C = cos(w * dotD + time * speed);
 
-        vec3 wavePosition = vec3(position.x + Q * A * C * D.x,
+        vec3 wavePosition = vec3(basePosition.x + Q * A * C * D.x,
                                  A * S,
-                                 position.z + Q * A * C * D.y);
+                                 basePosition.z + Q * A * C * D.y);
 
         vec3 waveNormal = vec3(-D.x * w * A * C,            
                                1.0 - Q * w * A * S,
@@ -130,13 +137,23 @@ const surface = new THREE.Mesh(
       }
 
       Wave sumWaves() {
-        Wave sum = Wave(vec3(0.0), vec3(0.0));
-        for (int i = 0; i < NUM_WAVEFRONTS; i++) {
-          Wave wave = getWave(wavefronts[i]);
-          sum.position += wave.position;
-          sum.normal += wave.normal;
+        Wave lowFrequencySum = Wave(vec3(0.0), vec3(0.0));
+        Wave highFrequencySum = Wave(vec3(0.0), vec3(0.0));
+
+        for (int i = 0; i < NUM_LF_WAVEFRONTS; i++) {
+          Wave wave = getWave(wavefronts[i], position);
+          lowFrequencySum.position += wave.position;
+          lowFrequencySum.normal += wave.normal;
         }
-        return sum;
+
+        for (int i = NUM_LF_WAVEFRONTS; i < NUM_HF_WAVEFRONTS; i++) {
+          Wave wave = getWave(wavefronts[i], lowFrequencySum.position);
+          highFrequencySum.position += wave.position;
+          highFrequencySum.normal += wave.normal;
+        }
+
+        return Wave(lowFrequencySum.position + highFrequencySum.position,
+                    lowFrequencySum.normal + highFrequencySum.normal);
       }
 
       void main(){
@@ -153,7 +170,7 @@ const surface = new THREE.Mesh(
       varying vec3 vertPos;
 
       const vec3 lightPos = vec3(1.0);
-      const vec3 diffuseColor = vec3(0.0, 0.0, 1.0);
+      const vec3 diffuseColor = vec3(0.1, 0.3, 0.6);
       const vec3 specularColor = vec3(1.0);
 
       void main() {
